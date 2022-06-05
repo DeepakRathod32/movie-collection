a742941fd034c38dff2b98fab90ad04f89fd80e0
deepakrathod32 <deepakrathod7132@gmail.com>
2022-06-05 19:28:01 +0530

    added UI and reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
+import {data } from './data';
 import React from 'react';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>app component</h1>
+      <Navbar />
+      <div className='main'>
+        <div className='tabs'>
+          <div className='tab'>Movies</div>
+          <div className='tab'>Favourites</div>
+        </div>
+
+        <div className='list'>
+          {data.map( movie => {
+            return <MovieCard movie={movie} />
+          })}
+        </div>
+      </div>
     </div>
   );
 }
