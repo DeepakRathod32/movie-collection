9d9f5c1e67ef6964833683beecf2e99150832e41
deepakrathod32 <deepakrathod7132@gmail.com>
2022-06-06 22:28:42 +0530

    added changing state

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,42 @@
-import {data } from './data';
-import React from 'react';
-import Navbar from './Navbar';
-import MovieCard from './MovieCard';
-import { addMovies } from '../actions';
-
+import { data } from "./data";
+import React from "react";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
 class App extends React.Component {
-  componentDidMount(){
+  componentDidMount() {
     const { store } = this.props;
     store.subscribe(() => {
-      console.log('Updated');
-      this.forceUpdate()
-    })
+      console.log("Updated");
+      this.forceUpdate();
+    });
     // Make an api call
     // dispatch action
-    store.dispatch(addMovies(data))
-    console.log('STATE', this.props.store.getState())
-
+    store.dispatch(addMovies(data));
+    console.log("STATE", this.props.store.getState());
   }
-  render(){
-    const movies = this.props.store.getState();
-    console.log('Rendered');
+  render() {
+    const { list } = this.props.store.getState(); // {list: [], favourites: []}
+    console.log("Rendered", this.props.store.getState());
     return (
       <div className="App">
         <Navbar />
-        <div className='main'>
-          <div className='tabs'>
-            <div className='tab'>Movies</div>
-            <div className='tab'>Favourites</div>
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
           </div>
-  
-          <div className='list'>
-            {movies.map( (movie, index) => {
-              return <MovieCard movie={movie} key={index}/>
+
+          <div className="list">
+            {list.map((movie, index) => {
+              return <MovieCard movie={movie} key={index} />;
             })}
           </div>
         </div>
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,16 @@
 import { ADD_MOVIES } from "../actions";
 
-export default function movies (state = [1], action){
+const initialMovieState = {
+    list: [],
+    favourites: []
+}
+
+export default function movies (state = initialMovieState, action){
     if(action.type === ADD_MOVIES){
-        return action.movies;
+        return {
+            ...state,
+            list: action.movies
+        };
     }
     return state;
 }
